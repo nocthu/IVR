bc1955c0393ff4c9c4649ffe4c9ea6cdabff20af
nocthu <vogelevs@gmail.com>
2020-11-25 23:02:36 +0300

    db-posts-new-3

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ class Posts(db.Model):  # бд постов
     user_id = db.Column(db.Integer)
     user_name = db.Column(db.String)
     user_surname = db.Column(db.String)
-    href_vk = db.Column(db.String)
-    href_telegram = db.Column(db.String)
-    href_google = db.Column(db.String)
+    # href_vk = db.Column(db.String)
+    # href_telegram = db.Column(db.String)
+    # href_google = db.Column(db.String)
     post_type = db.Column(db.Integer) # для себя - 1, вакансия - 2, опрос - 3, идея - 4
     project_type = db.Column(db.String) # тип ИВР
     subject = db.Column(db.String) # предмет ИВР
@@ -65,15 +65,15 @@ class Posts(db.Model):  # бд постов
     # post_contacts = db.Column(db.String)
     # post_date = db.Column(db.String)
 
-    def __init__(self, user_id, user_name, user_surname, href_vk, href_telegram, href_google, 
+    def __init__(self, user_id, user_name, user_surname, 
                     post_type, project_type, subject, problem_type, name, demands, description, 
-                    href_vk, href_google, href_quiz, date):
+                    href_vk, href_telegram, href_google, href_quiz, date):
         self.user_id = user_id
         self.user_name = user_name
         self.user_surname = user_surname
-        self.href_vk = href_vk
-        self.href_telegram = href_telegram
-        self.href_google = href_google
+        # self.href_vk = href_vk
+        # self.href_telegram = href_telegram
+        # self.href_google = href_google
         self.post_type = post_type
         self.project_type = project_type
         self.subject = subject
